01b09a8d745e9d2cf6dcf9c448ca17e011165686
pumuckelo <46658609+pumuckelo@users.noreply.github.com>
2019-12-17 16:11:57 +0100

    format

--- frontend/src/components/ProjectList/Project/TodoList/TodoItem/TodoItem.jsx
+++ frontend/src/components/ProjectList/Project/TodoList/TodoItem/TodoItem.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './TodoItem.css'
 
-const TodoItem = (props)=>{
+const TodoItem = (props) => {
 
 
     return (
-        <div  className={props.completed ? "todoitem completed" : "todoitem"}>
+        <div className={props.completed ? "todoitem completed" : "todoitem"}>
             <p onClick={props.toggleCompleted}>{props.title}</p>
 
             <i onClick={props.deleteTodo} className="fas fa-trash-alt"></i>
--- frontend/src/components/ProjectList/Project/TodoList/TodoList.jsx
+++ frontend/src/components/ProjectList/Project/TodoList/TodoList.jsx
@@ -2,47 +2,57 @@ import React, {useRef, useState} from 'react'
 import './TodoList.css'
 
 import TodoItem from "./TodoItem/TodoItem";
-const TodoList = (props)=>{
 
-    const [todos, setTodos] = useState([{title: "App designen", completed: true},{title: "App strukturieren", completed: false},{title: "Datenbank planen", completed: false},{title: "Backend planen", completed: false},])
+const TodoList = (props) => {
+
+    const [todos, setTodos] = useState([{title: "App designen", completed: true}, {
+        title: "App strukturieren",
+        completed: false
+    }, {title: "Datenbank planen", completed: false}, {title: "Backend planen", completed: false},])
     const newTodoInput = useRef('')
 
 
-   const newTodoSubmitHandler = (event) => {
-       event.preventDefault()
+    const newTodoSubmitHandler = (event) => {
+        event.preventDefault()
         let newTodos = [...todos]
-       newTodos.push({title: newTodoInput.current.value, completed: false})
-       setTodos(newTodos)
-       newTodoInput.current.value = ''
+        newTodos.push({title: newTodoInput.current.value, completed: false})
+        setTodos(newTodos)
+        newTodoInput.current.value = ''
     }
 
-    const deleteTodoHandler = (index) =>{
-           let newTodos = [...todos]
-            newTodos.splice(index, 1)
+    const deleteTodoHandler = (index) => {
+        let newTodos = [...todos]
+        newTodos.splice(index, 1)
         console.log(index)
         console.log(newTodos)
         console.log("tried to delet")
         setTodos(newTodos)
     }
 
-    const toggleTodoCompletedHandler = (index) =>{
-            let newTodos = [...todos]
-            newTodos[index].completed = !newTodos[index].completed
-            setTodos(newTodos)
+    const toggleTodoCompletedHandler = (index) => {
+        let newTodos = [...todos]
+        newTodos[index].completed = !newTodos[index].completed
+        setTodos(newTodos)
     }
 
 
-    const todoItems = todos.map((todoitem, index)=>{
-       return <TodoItem deleteTodo={()=>{deleteTodoHandler(index)}} toggleCompleted={()=>{toggleTodoCompletedHandler(index)}} key={index} title={todoitem.title} completed={todoitem.completed}/>
+    const todoItems = todos.map((todoitem, index) => {
+        return <TodoItem deleteTodo={() => {
+            deleteTodoHandler(index)
+        }} toggleCompleted={() => {
+            toggleTodoCompletedHandler(index)
+        }} key={index} title={todoitem.title} completed={todoitem.completed}/>
     })
     return (
         <div className='todolist'>
             <h2>{props.title}</h2>
             {todoItems}
-            <form className='newTodoForm' onSubmit={(event) => {newTodoSubmitHandler(event)}} action="">
+            <form className='newTodoForm' onSubmit={(event) => {
+                newTodoSubmitHandler(event)
+            }} action="">
                 <div className='newTodoForm-control'>
-                <input ref={newTodoInput} type="text" placeholder='New todo' />
-                <button type='submit'>+</button>
+                    <input ref={newTodoInput} type="text" placeholder='New todo'/>
+                    <button type='submit'>+</button>
                 </div>
             </form>
         </div>
