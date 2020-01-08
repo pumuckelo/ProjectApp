6730e87960b033b734c0e0a10d0ef81136ec43b4
pumuckelo <46658609+pumuckelo@users.noreply.github.com>
2020-01-08 20:54:19 +0100

    added following functions:
    added following functions:
    
    Add new Todo
    Toggle Project List, Designed Project List
    Show the Todo Items in Todo List

--- frontend/src/components/ProjectList/Project/Project.jsx
+++ frontend/src/components/ProjectList/Project/Project.jsx
@@ -1,65 +1,71 @@
-import React, {useState, useRef, Fragment} from 'react'
-import './Project.css'
+import React, { useState, useRef, Fragment } from "react";
+import "./Project.css";
 import TodoList from "./TodoList/TodoList";
 import ProjectList from "../ProjectList";
 
-const Project = (props) => {
+const Project = props => {
+  const newListInput = useRef("");
 
-    const newListInput = useRef('')
+  const [todoLists, setTodoLists] = useState([]);
+  const [projectListEnabled, setProjectListEnabled] = useState(false);
 
-    const [todoLists, setTodoLists] = useState(['Erste Phase', 'Zweite Phase'])
-    const [projectListEnabled, setProjectListEnabled] = useState(false)
+  const toggleProjectList = () => {
+    setProjectListEnabled(!projectListEnabled);
+  };
+  const addNewTodoListHandler = event => {
+    event.preventDefault();
+    let updatedTodoLists = [...todoLists];
+    updatedTodoLists.push(newListInput.current.value);
+    setTodoLists(updatedTodoLists);
+    newListInput.current.value = "";
+  };
 
+  const todoListsComponents = todoLists.map((todoList, index) => {
+    return <TodoList title={todoList} key={index} />;
+  });
+  //graphql query to get infos about project with the id you get from props.projectid
+  // then create array of todolists
 
-    const toggleProjectList = () => {
-        setProjectListEnabled(!projectListEnabled)
-    }
-    const addNewTodoListHandler = (event) => {
-        event.preventDefault()
-        let updatedTodoLists = [...todoLists]
-        updatedTodoLists.push(newListInput.current.value)
-        setTodoLists(updatedTodoLists)
-        newListInput.current.value = ''
+  return (
+    <Fragment>
+      {projectListEnabled && (
+        <ProjectList
+          close={() => {
+            toggleProjectList();
+          }}
+        />
+      )}
+      <div className="project">
+        <div className="heading">
+          <i
+            onClick={() => {
+              toggleProjectList();
+            }}
+            className="fas fa-ellipsis-v fa-lg"
+          ></i>
+          <h1>{props.title}</h1>
+        </div>
+        <div className="todolists">
+          {/*Hier würden normalerweise die todolists von der datenbank hinkommen in einem array.map => todolist*/}
+          {todoListsComponents}
 
-    }
-
-    const todoListsComponents = todoLists.map((todoList, index) => {
-        return <TodoList title={todoList} key={index}/>
-    })
-//graphql query to get infos about project with the id you get from props.projectid
-    // then create array of todolists
-
-    return (
-        <Fragment>
-            {projectListEnabled && <ProjectList close={() => {
-                toggleProjectList()
-            }}/>}
-            <div className='project'>
-                <div className='heading'>
-                    <i onClick={() => {
-                        toggleProjectList()
-                    }} className="fas fa-ellipsis-v fa-lg"></i>
-                    <h1>{props.title}</h1>
-
-                </div>
-                <div className='todolists'>
-                    {/*Hier würden normalerweise die todolists von der datenbank hinkommen in einem array.map => todolist*/}
-                    {todoListsComponents}
-
-                    <form onSubmit={(event) => {
-                        addNewTodoListHandler(event)
-                    }} action="">
-                        <div className='newListControl'>
-                            <input ref={newListInput} type='text' placeholder="List Name"/>
-                            <button className='newList'>New <i className="fas fa-clipboard-list"></i></button>
-                        </div>
-                    </form>
-
-                </div>
+          <form
+            onSubmit={event => {
+              addNewTodoListHandler(event);
+            }}
+            action=""
+          >
+            <div className="newListControl">
+              <input ref={newListInput} type="text" placeholder="New List" />
+              <button className="newList">
+                Add <i className="fas fa-clipboard-list"></i>
+              </button>
             </div>
-        </Fragment>
-    )
-}
+          </form>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
-
-export default Project
+export default Project;
--- frontend/src/components/ProjectList/Project/TodoList/TodoList.jsx
+++ frontend/src/components/ProjectList/Project/TodoList/TodoList.jsx
@@ -1,63 +1,89 @@
-import React, {useRef, useState} from 'react'
-import './TodoList.css'
+import React, { useRef, useState, Fragment } from "react";
+import "./TodoList.css";
 
 import TodoItem from "./TodoItem/TodoItem";
+import TodoListSettings from "./TodoListSettings/TodoListSettings";
 
-const TodoList = (props) => {
+const TodoList = props => {
+  const [todos, setTodos] = useState([]);
+  const [settingsVisible, setSettingsVisible] = useState(false);
+  const newTodoInput = useRef("");
 
-    const [todos, setTodos] = useState([])
-    const newTodoInput = useRef('')
-
-    /*{title: "App designen", completed: true}, {
+  /*{title: "App designen", completed: true}, {
             title: "App strukturieren",
             completed: false
         }, {title: "Datenbank planen", completed: false}, {title: "Backend planen", completed: false},*/
-    const newTodoSubmitHandler = (event) => {
-        event.preventDefault()
-        let newTodos = [...todos]
-        newTodos.push({title: newTodoInput.current.value, completed: false})
-        setTodos(newTodos)
-        newTodoInput.current.value = ''
+  const newTodoSubmitHandler = event => {
+    event.preventDefault();
+    let newTodos = [...todos];
+    if (newTodoInput.current.value !== "") {
+      newTodos.push({ title: newTodoInput.current.value, completed: false });
+      setTodos(newTodos);
     }
+    newTodoInput.current.value = "";
+  };
 
-    const deleteTodoHandler = (index) => {
-        let newTodos = [...todos]
-        newTodos.splice(index, 1)
-        console.log(index)
-        console.log(newTodos)
-        console.log("tried to delet")
-        setTodos(newTodos)
-    }
+  const deleteTodoHandler = index => {
+    let newTodos = [...todos];
+    newTodos.splice(index, 1);
+    console.log(index);
+    console.log(newTodos);
+    console.log("tried to delet");
+    setTodos(newTodos);
+  };
 
-    const toggleTodoCompletedHandler = (index) => {
-        let newTodos = [...todos]
-        newTodos[index].completed = !newTodos[index].completed
-        setTodos(newTodos)
-    }
+  const toggleTodoCompletedHandler = index => {
+    let newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
 
+  const toggleListSettingsHandler = () => {
+    setSettingsVisible(!settingsVisible);
+  };
 
-    const todoItems = todos.map((todoitem, index) => {
-        return <TodoItem deleteTodo={() => {
-            deleteTodoHandler(index)
-        }} toggleCompleted={() => {
-            toggleTodoCompletedHandler(index)
-        }} key={index} title={todoitem.title} completed={todoitem.completed}/>
-    })
+  const todoItems = todos.map((todoitem, index) => {
     return (
-        <div className='todolist'>
-            <i className="fas fa-cog"></i>
-            <h2>{props.title}</h2>
-            {todoItems}
-            <form className='newTodoForm' onSubmit={(event) => {
-                newTodoSubmitHandler(event)
-            }} action="">
-                <div className='newTodoForm-control'>
-                    <input ref={newTodoInput} type="text" placeholder='New todo'/>
-                    <button type='submit'>+</button>
-                </div>
-            </form>
-        </div>
-    )
-}
+      <TodoItem
+        deleteTodo={() => {
+          deleteTodoHandler(index);
+        }}
+        toggleCompleted={() => {
+          toggleTodoCompletedHandler(index);
+        }}
+        key={index}
+        title={todoitem.title}
+        completed={todoitem.completed}
+      />
+    );
+  });
+  return (
+    <Fragment>
+      {settingsVisible && <TodoListSettings listname={props.title} />}
+      <div className="todolist">
+        <i
+          onClick={() => {
+            toggleListSettingsHandler();
+          }}
+          className="fas fa-cog"
+        ></i>
+        <h2>{props.title}</h2>
+        {todoItems}
+        <form
+          className="newTodoForm"
+          onSubmit={event => {
+            newTodoSubmitHandler(event);
+          }}
+          action=""
+        >
+          <div className="newTodoForm-control">
+            <input ref={newTodoInput} type="text" placeholder="New todo" />
+            <button type="submit">+</button>
+          </div>
+        </form>
+      </div>
+    </Fragment>
+  );
+};
 
-export default TodoList
+export default TodoList;
