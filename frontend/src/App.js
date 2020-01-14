44d0ebac59bbb39a4a3aefa8b0e6be1b9a65507a
pumuckelo <46658609+pumuckelo@users.noreply.github.com>
2020-01-14 20:03:33 +0100

    Added Graphql subscription functionality to frontend (Web Socket Link)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,21 +7,43 @@ import Home from "./components/Home/Home";
 import AuthContext from "./context/auth-context";
 import Cookies from "js-cookie";
 
-import {
-  InMemoryCache,
-  HttpLink,
-  ApolloClient,
-  ApolloProvider
-} from "@apollo/client";
+import { InMemoryCache, ApolloClient, ApolloProvider } from "@apollo/client";
+
+import { WebSocketLink } from "apollo-link-ws";
+import { split } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import { getMainDefinition } from "apollo-utilities";
+
 import Login from "./components/Authentication/Login/Login";
 import Navbar from "./components/Navbar/Navbar";
 
+const httpLink = new HttpLink({
+  uri: "http://localhost:12000/graphql",
+  credentials: "include"
+});
+
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:12000/graphql",
+  options: {
+    reconnect: true
+  }
+});
+
+//split link so it sends subscriptions to subscription link and http operations
+// to the http link
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  httpLink
+);
+
 const gqlClient = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: "http://localhost:12000/graphql",
-    credentials: "include"
-  })
+  link: link
 });
 
 function App() {
